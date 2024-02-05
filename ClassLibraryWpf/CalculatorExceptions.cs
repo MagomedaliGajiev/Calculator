a89064c28a460c032f3828debee2de5f0ed2a465
Magomedali Gajiev <mag198421@gmail.com>
2024-02-05 18:38:15 +0300

    Изменил класс CalculatorExceptions

--- ClassLibraryWpf/CalculatorExceptions.cs
+++ ClassLibraryWpf/CalculatorExceptions.cs
@@ -10,15 +10,63 @@ namespace ClassLibraryWpf
 {
     public class CalculatorExceptions : CalculatorArithmetic
     {
+        public override void Add(int value)
+        {
+            try
+            {
+                base.Add(value);
+            }
+            catch (Exception  e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        public override void Sub(int value)
+        {
+            try
+            {
+                base.Sub(value);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        public override void Mult(int value)
+        {
+            try
+            {
+                base.Mult(value);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
         public override void Div(int value)
         {
             try
             {
                 base.Div(value);
             }
-            catch (DivideByZeroException e)
+            catch (Exception e)
             {
                 MessageBox.Show(e.Message);
+            }
+        }
+
+        public override void Cancel()
+        {
+            try
+            {   
+                base.Cancel();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                throw;
             }
         }
     }
